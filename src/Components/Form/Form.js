c2568473fc310c5efd73fea15be9f2357861af51
leonid <leonid.karachevtsev@gmail.com>
2019-02-17 21:49:50 +0300

    add form registration

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -3,6 +3,20 @@ import './Form.css';
 
 class Form extends Component {
   render() {
+    if (this.props.type === 'reg') {
+      return (
+        <form className="reg-form">
+          <input type="text" name="name" className="input-default" placeholder="Name" />
+          <input type="text" name="login" className="input-default" placeholder="Login" />
+          <input type="email" name="email" className="input-default" placeholder="E-Mail" />
+          <input type="password" name="password" className="input-default" placeholder="Password" />
+          <div className="ws-flexbox button-form">
+            <button type="submit" name="continue" className="button-login">Continue</button>
+            <a href="/" className="button-register">Login</a>
+          </div>
+        </form>
+      );
+    }
     return (
       <form className="login-form">
         <input type="text" name="login" className="input-default" placeholder="Login" />
